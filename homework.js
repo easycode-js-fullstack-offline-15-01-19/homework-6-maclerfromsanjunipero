466d51af8c28096d33c95c2c0e3b8badc250a7ab
maclerfromsanjunipero <vasilenko.maxim5@gmail.com>
2019-02-11 22:35:50 +0200

    Vasilenko Max HW#6

--- homework.js
+++ homework.js
@@ -1,2 +1,60 @@
-// -------------------------- Home work --------------------------
-// -------------------------- ФИО --------------------------
+// -------------------------- Home work#6 --------------------------
+// -------------------------- Vasilenko Max --------------------------
+
+//--Events.Tasks
+
+//--Task#1
+ let btnMsg = document.getElementById("btn-msg");
+
+ btnMsg.addEventListener('click', function () {
+     alert(btnMsg.dataset.text)
+ });
+
+
+//--Task#2
+btnMsg.onmouseover = function () {
+    btnMsg.setAttribute('style','background-color:red;');
+};
+btnMsg.onmouseout = function () {
+    btnMsg.removeAttribute('style');
+};
+
+//--Task#3
+document.addEventListener('click',function (e) {
+    document.querySelector("#tag").textContent = 'Tag: '+ e.target.tagName;
+});
+
+//--Task#4
+let btnGen = document.getElementById("btn-generate");
+
+btnGen.addEventListener('click', function () {
+    let findUl = document.querySelector('ul');
+    let createLi = document.createElement('li');
+    let textLi = 'item ' + (findUl.children.length + 1);
+
+    createLi.appendChild(document.createTextNode(textLi));
+    findUl.appendChild(createLi);
+});
+
+//--Task#6
+
+let dropDownItem = document.querySelectorAll('.dropdown-item');
+let menuHide = document.getElementsByClassName('dropdown-menu');
+for (let i = 0; i < dropDownItem.length; i++){
+    dropDownItem[i].setAttribute('id', 'menu-click'+[i]);
+    menuHide[i].setAttribute('id', 'menu-hide'+[i]);
+}
+
+let menuClickFirst = document.getElementById('menu-click0');
+let menuClickSecond = document.getElementById('menu-click1');
+
+menuClickFirst.addEventListener('click',function () {
+    document.getElementById('menu-hide0').classList.toggle('d-none');
+    document.getElementById('menu-hide1').classList.add('d-none');
+});
+
+menuClickSecond.addEventListener('click',function () {
+    document.getElementById('menu-hide1').classList.toggle('d-none');
+    document.getElementById('menu-hide0').classList.add('d-none');
+});
+
